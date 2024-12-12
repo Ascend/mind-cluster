77e4e75ac57b108fe7491dabbcda272400704924
郭鹏鑫 <1215942406@qq.com>
2024-12-12 14:15:37 +0800

    【修改说明】device-plugin frequency

--- component/ascend-device-plugin/pkg/common/fault_code_test.go
+++ component/ascend-device-plugin/pkg/common/fault_code_test.go
@@ -1421,6 +1421,7 @@ func TestGetNetworkFaultType(t *testing.T) {
 				},
 			},
 		}
+		convey.So(GetNetworkFaultType(faultCodes, logicId), convey.ShouldEqual, ManuallySeparateNPU)
 		convey.So(GetNetworkFaultType(faultCodes, logicId), convey.ShouldEqual, PreSeparateNPU)
 	})
 }
