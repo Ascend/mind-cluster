d5a65d0647c241f81f00fac564f85e809546340f
郭鹏鑫 <1215942406@qq.com>
2024-12-28 14:57:42 +0800

    【修改说明】device-plugin server 补充DT用例

--- component/ascend-device-plugin/pkg/server/manager_test.go
+++ component/ascend-device-plugin/pkg/server/manager_test.go
@@ -214,16 +214,16 @@ func TestGetNewNodeLabel(t *testing.T) {
 				return common.Infer
 			})
 		defer mockGetDeviceUsage.Reset()
-		mockGetValidChipInfo := gomonkey.ApplyMethod(reflect.TypeOf(new(devmanager.DeviceManagerMock)),
-			"GetValidChipInfo", func(_ *devmanager.DeviceManagerMock, _ int32) (npuCommon.BoardInfo, error) {
+		mockGetBoardInfo := gomonkey.ApplyMethod(reflect.TypeOf(new(devmanager.DeviceManagerMock)),
+			"GetBoardInfo", func(_ *devmanager.DeviceManagerMock, _ int32) (npuCommon.BoardInfo, error) {
 				return npuCommon.BoardInfo{BoardId: common.A300IA2BoardId}, nil
 			})
-		defer mockGetValidChipInfo.Reset()
-		mockGetBoardInfo := gomonkey.ApplyMethod(reflect.TypeOf(new(devmanager.DeviceManagerMock)),
-			"GetBoardInfo", func(_ *devmanager.DeviceManagerMock) (npuCommon.ChipInfo, error) {
+		defer mockGetBoardInfo.Reset()
+		mockGetValidChipInfo := gomonkey.ApplyMethod(reflect.TypeOf(new(devmanager.DeviceManagerMock)),
+			"GetValidChipInfo", func(_ *devmanager.DeviceManagerMock) (npuCommon.ChipInfo, error) {
 				return npuCommon.ChipInfo{Name: "testName"}, nil
 			})
-		defer mockGetBoardInfo.Reset()
+		defer mockGetValidChipInfo.Reset()
 		mockIsContainAll300IDuo := gomonkey.ApplyFuncReturn(common.IsContainAll300IDuo, true)
 		defer mockIsContainAll300IDuo.Reset()
 		labelMap, err := hdm.getNewNodeLabel(testNode)
