b270c67b18ef9a49ef14940362d133584f1bc8bc
lirui238 <2396601465@qq.com>
2025-01-26 03:36:46 +0000

    !424 【修改说明】[clusterd]faultmanager重构-增加fault_utils_test * 【修改说明】[clusterd]faultmanager重构-增加fault_utils_test

---
+++ component/clusterd/pkg/domain/faultdomain/fault_utils_test.go
@@ -0,0 +1,508 @@
+// Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+
+// Package faultmanager contain fault process
+package faultdomain
+
+import (
+	"fmt"
+	"reflect"
+	"sort"
+	"testing"
+	"time"
+
+	"github.com/agiledragon/gomonkey/v2"
+	v1 "k8s.io/api/core/v1"
+
+	"ascend-common/common-utils/hwlog"
+	"clusterd/pkg/common/constant"
+	"clusterd/pkg/common/util"
+)
+
+var (
+	JobId          = "JobId"
+	NodeName       = "Node"
+	Time100Seconds = 100 * time.Second.Milliseconds()
+	Time1Seconds   = 1 * time.Second.Milliseconds()
+	DeviceId       = "0"
+	RankID         = "8"
+	CmName         = "mindx-dl-deviceinfo-" + NodeName
+	DeviceName     = constant.Ascend910Server + "-" + DeviceId
+	JobServerMap   = constant.JobServerInfoMap{
+		InfoMap: map[string]map[string]constant.ServerHccl{
+			JobId: {
+				NodeName: {
+					DeviceList: []constant.Device{{
+						DeviceID: DeviceId,
+						RankID:   RankID,
+					}},
+					ServerName: NodeName,
+				},
+			},
+		},
+	}
+	OriginalDeviceCm = &constant.DeviceInfo{
+		CmName: CmName,
+		DeviceInfoNoName: constant.DeviceInfoNoName{
+			DeviceList: map[string]string{
+				"huawei.com/Ascend910-Fault": `
+[
+  {
+    "fault_type": "CardUnhealthy",
+	"fault_code": "1801   ,  1809  ",
+    "fault_time_and_level_map":
+      {
+        "1801": {"fault_time":1234, "fault_level": "RestartBusiness"}, 
+        "1809": {"fault_time":5678, "fault_level": "NotHandleFault"}
+      },"npu_name": "Ascend910-0"
+  }
+]`,
+			},
+		},
+	}
+)
+
+func TestMain(m *testing.M) {
+	hwLogConfig := &hwlog.LogConfig{LogFileName: "../../../../testdata/clusterd.log"}
+	hwLogConfig.MaxBackups = 30
+	hwLogConfig.MaxAge = 7
+	hwLogConfig.LogLevel = 0
+	if err := hwlog.InitRunLogger(hwLogConfig, nil); err != nil {
+		fmt.Printf("hwlog init failed, error is %v\n", err)
+		return
+	}
+	m.Run()
+}
+
+func TestSplitDeviceFault(t *testing.T) {
+	t.Run("TestSplitDeviceFault", func(t *testing.T) {
+		npuName := "Ascend910-0"
+		var faultInfo = constant.DeviceFault{
+			NPUName:   npuName,
+			FaultCode: "0x1,0x2",
+			FaultTimeAndLevelMap: map[string]constant.FaultTimeAndLevel{
+				"0x1": {FaultLevel: constant.NotHandleFault, FaultTime: 1},
+				"0x2": {FaultLevel: constant.SubHealthFault, FaultTime: 1},
+			},
+		}
+
+		got := splitDeviceFault(faultInfo, "node1")
+		want := []constant.DeviceFault{
+			{
+				NPUName:              npuName,
+				FaultCode:            "0x1",
+				FaultLevel:           constant.NotHandleFault,
+				LargeModelFaultLevel: constant.NotHandleFault,
+				FaultHandling:        constant.NotHandleFault,
+				FaultTimeAndLevelMap: map[string]constant.FaultTimeAndLevel{
+					"0x1": {FaultLevel: constant.NotHandleFault, FaultTime: 1},
+				},
+			}, {
+				NPUName:              npuName,
+				FaultCode:            "0x2",
+				FaultLevel:           constant.SubHealthFault,
+				LargeModelFaultLevel: constant.SubHealthFault,
+				FaultHandling:        constant.SubHealthFault,
+				FaultTimeAndLevelMap: map[string]constant.FaultTimeAndLevel{
+					"0x2": {FaultLevel: constant.SubHealthFault, FaultTime: 1},
+				},
+			},
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("splitDeviceFault() = %v, want %v", got, want)
+		}
+	})
+}
+
+// TestSplitDeviceFaultWithManuallySeparateFaultLevel should split out a DeviceFault as middle data,
+// when dp report constant.ManuallySeparateNPU
+func TestSplitDeviceFaultWithManuallySeparateFaultLevel(t *testing.T) {
+	t.Run("TestSplitDeviceFaultWithManuallySeparateFaultLevel", func(t *testing.T) {
+		npuName := "Ascend910-0"
+		var faultInfo = constant.DeviceFault{
+			NPUName:              npuName,
+			FaultCode:            "",
+			FaultLevel:           constant.ManuallySeparateNPU,
+			FaultTimeAndLevelMap: map[string]constant.FaultTimeAndLevel{},
+		}
+
+		got := splitDeviceFault(faultInfo, "node1")
+		want := []constant.DeviceFault{
+			{
+				NPUName:              npuName,
+				FaultCode:            constant.ManuallySeparateNPU,
+				FaultLevel:           constant.ManuallySeparateNPU,
+				LargeModelFaultLevel: constant.ManuallySeparateNPU,
+				FaultHandling:        constant.ManuallySeparateNPU,
+				FaultTimeAndLevelMap: map[string]constant.FaultTimeAndLevel{
+					constant.ManuallySeparateNPU: {
+						FaultTime:  constant.UnknownFaultTime,
+						FaultLevel: constant.ManuallySeparateNPU,
+					},
+				},
+			},
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("splitDeviceFault() = %v, want %v", got, want)
+		}
+	})
+}
+
+// TestMergeSameTypeDeviceFault should be merged, when fault type is same
+func TestMergeSameTypeDeviceFault(t *testing.T) {
+	t.Run("Test_mergeDeviceFault", func(t *testing.T) {
+		npuName := "Ascend910-0"
+		split := []constant.DeviceFault{
+			{
+				FaultType:  constant.CardUnhealthy,
+				NPUName:    npuName,
+				FaultCode:  "0x1",
+				FaultLevel: constant.NotHandleFault,
+				FaultTimeAndLevelMap: map[string]constant.FaultTimeAndLevel{
+					"0x1": {FaultLevel: constant.NotHandleFault, FaultTime: 1},
+					"0x2": {FaultLevel: constant.SubHealthFault, FaultTime: 1},
+				},
+			},
+			{
+				FaultType:  constant.CardUnhealthy,
+				NPUName:    npuName,
+				FaultCode:  "0x2",
+				FaultLevel: constant.SubHealthFault,
+				FaultTimeAndLevelMap: map[string]constant.FaultTimeAndLevel{
+					"0x1": {FaultLevel: constant.NotHandleFault, FaultTime: 1},
+					"0x2": {FaultLevel: constant.SubHealthFault, FaultTime: 1},
+				},
+			},
+		}
+		want := []constant.DeviceFault{
+			{
+				FaultType:            constant.CardUnhealthy,
+				NPUName:              npuName,
+				FaultCode:            "0x1,0x2",
+				FaultLevel:           constant.SubHealthFault,
+				LargeModelFaultLevel: constant.SubHealthFault,
+				FaultHandling:        constant.SubHealthFault,
+				FaultTimeAndLevelMap: map[string]constant.FaultTimeAndLevel{
+					"0x1": {FaultLevel: constant.NotHandleFault, FaultTime: 1},
+					"0x2": {FaultLevel: constant.SubHealthFault, FaultTime: 1},
+				},
+			},
+		}
+		got, err := mergeDeviceFault(split)
+		if err != nil {
+			t.Errorf("mergeDeviceFault() error = %v", err)
+		}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("mergeDeviceFault() got = %v, want %v", util.ObjToString(got), util.ObjToString(want))
+		}
+	})
+}
+
+// TestMergeDifferentTypeDeviceFault should not be merged, when fault type isn't same
+func TestMergeDifferentTypeDeviceFault(t *testing.T) {
+	t.Run("Test_mergeDeviceFault", func(t *testing.T) {
+		npuName := "Ascend910-0"
+		split := []constant.DeviceFault{
+			{
+				FaultType:            constant.CardUnhealthy,
+				NPUName:              npuName,
+				FaultCode:            "0x1",
+				FaultLevel:           constant.NotHandleFault,
+				LargeModelFaultLevel: constant.NotHandleFault,
+				FaultHandling:        constant.NotHandleFault,
+				FaultTimeAndLevelMap: map[string]constant.FaultTimeAndLevel{
+					"0x1": {FaultLevel: constant.NotHandleFault, FaultTime: 1},
+				},
+			},
+			{
+				FaultType:            constant.CardNetworkUnhealthy,
+				NPUName:              npuName,
+				FaultCode:            "0x2",
+				FaultLevel:           constant.SubHealthFault,
+				LargeModelFaultLevel: constant.SubHealthFault,
+				FaultHandling:        constant.SubHealthFault,
+				FaultTimeAndLevelMap: map[string]constant.FaultTimeAndLevel{
+					"0x2": {FaultLevel: constant.SubHealthFault, FaultTime: 1},
+				},
+			},
+		}
+		got, err := mergeDeviceFault(split)
+		if err != nil {
+			t.Errorf("mergeDeviceFault() error = %v", err)
+		}
+		sort.Slice(got, func(i, j int) bool {
+			return got[i].FaultType > got[j].FaultType
+		})
+		if !reflect.DeepEqual(got, split) {
+			t.Errorf("mergeDeviceFault() got = %v, want %v", util.ObjToString(got), util.ObjToString(split))
+		}
+	})
+}
+
+// TestMergeconstant.ManuallySeparateNPUTypeDeviceFault should combine other fault info and constant.ManuallySeparateNPU.
+func TestMergeManuallySeparateNPUTypeDeviceFault(t *testing.T) {
+	t.Run("TestMergeconstant.ManuallySeparateNPUTypeDeviceFault", func(t *testing.T) {
+		npuName := "Ascend910-0"
+		split := []constant.DeviceFault{
+			{
+				FaultType:            constant.CardUnhealthy,
+				NPUName:              npuName,
+				FaultCode:            constant.ManuallySeparateNPU,
+				FaultLevel:           constant.ManuallySeparateNPU,
+				FaultTimeAndLevelMap: map[string]constant.FaultTimeAndLevel{},
+			},
+			{
+				FaultType:  constant.CardUnhealthy,
+				NPUName:    npuName,
+				FaultCode:  constant.UceFaultCode,
+				FaultLevel: constant.RestartBusiness,
+				FaultTimeAndLevelMap: map[string]constant.FaultTimeAndLevel{
+					constant.UceFaultCode: {
+						FaultTime:  constant.UnknownFaultTime,
+						FaultLevel: constant.RestartBusiness,
+					},
+				},
+			},
+		}
+		got, err := mergeDeviceFault(split)
+		if err != nil {
+			t.Errorf("mergeDeviceFault() error = %v", err)
+		}
+		want := []constant.DeviceFault{
+			{
+				FaultType:            constant.CardUnhealthy,
+				NPUName:              npuName,
+				FaultCode:            constant.UceFaultCode,
+				FaultLevel:           constant.ManuallySeparateNPU,
+				LargeModelFaultLevel: constant.ManuallySeparateNPU,
+				FaultHandling:        constant.ManuallySeparateNPU,
+				FaultTimeAndLevelMap: map[string]constant.FaultTimeAndLevel{
+					constant.UceFaultCode: {
+						FaultTime:  constant.UnknownFaultTime,
+						FaultLevel: constant.RestartBusiness,
+					},
+				},
+			},
+		}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("TestMergeconstant.ManuallySeparateNPUTypeDeviceFault() got = %v, want %v", util.ObjToString(got), util.ObjToString(want))
+		}
+	})
+}
+
+func TestGetAdvanceDeviceCm(t *testing.T) {
+	advanceDeviceCm := GetAdvanceDeviceCm(OriginalDeviceCm)
+	if len(advanceDeviceCm.FaultDeviceList[DeviceName]) != 2 {
+		t.Errorf("TestGetAdvanceDeviceCm failed")
+		return
+	}
+	faultTimeAndLevel, ok := advanceDeviceCm.FaultDeviceList[DeviceName][0].FaultTimeAndLevelMap["1801"]
+	if !ok || faultTimeAndLevel.FaultTime != 1234 || faultTimeAndLevel.FaultLevel != "RestartBusiness" {
+		t.Errorf("TestGetAdvanceDeviceCm failed")
+		return
+	}
+}
+
+func TestValidBusinessUceReportInfo(t *testing.T) {
+	t.Run("TestValidBusinessUceReportInfo", func(t *testing.T) {
+		reportInfo := &constant.ReportInfo{
+			RecoverTime:  Time100Seconds - Time1Seconds,
+			CompleteTime: 0,
+		}
+		mockTime := time.Time{}
+		mockUnixMilli := gomonkey.ApplyPrivateMethod(mockTime, "UnixMilli", func() int64 {
+			return Time100Seconds
+		})
+		mockNow := gomonkey.ApplyFunc(time.Now, func() time.Time {
+			return mockTime
+		})
+		defer func() {
+			mockNow.Reset()
+			mockUnixMilli.Reset()
+		}()
+		if !ValidBusinessUceReportInfo(reportInfo) {
+			t.Error("TestValidBusinessUceReportInfo fail")
+		}
+		reportInfo.RecoverTime = 0
+		if ValidBusinessUceReportInfo(reportInfo) {
+			t.Error("TestValidBusinessUceReportInfo fail")
+		}
+	})
+}
+
+func TestCanDoStepRetry(t *testing.T) {
+	uceDeviceInfo := &constant.UceDeviceInfo{
+		DeviceName:   DeviceName,
+		FaultTime:    Time100Seconds,
+		RecoverTime:  Time100Seconds + Time1Seconds,
+		CompleteTime: 0,
+	}
+	t.Run("TestCanDoStepRetry", func(t *testing.T) {
+		mockTime := time.Time{}
+		mockUnixMilli := gomonkey.ApplyPrivateMethod(mockTime, "UnixMilli", func() int64 {
+			return Time100Seconds + 20*Time1Seconds
+		})
+		mockNow := gomonkey.ApplyFunc(time.Now, func() time.Time {
+			return mockTime
+		})
+		defer func() {
+			mockNow.Reset()
+			mockUnixMilli.Reset()
+		}()
+		if !CanDoStepRetry(uceDeviceInfo) {
+			t.Error("TestCanDoStepRetry fail")
+		}
+	})
+}
+
+func TestGetContainedElementIdx(t *testing.T) {
+	arr := []string{"1", "2"}
+	t.Run("TestGetContainedElementIdx", func(t *testing.T) {
+		if got := GetContainedElementIdx("1", arr); got != 0 {
+			t.Error("GetContainedElementIdx() fail")
+		}
+		if got := GetContainedElementIdx("3", arr); got != -1 {
+			t.Error("GetContainedElementIdx() fail")
+		}
+	})
+}
+
+func TestGetFaultTime(t *testing.T) {
+	fault := constant.DeviceFault{
+		FaultCode: constant.UceFaultCode,
+		FaultTimeAndLevelMap: map[string]constant.FaultTimeAndLevel{
+			constant.UceFaultCode: {
+				FaultTime:  Time100Seconds,
+				FaultLevel: constant.RestartBusiness,
+			},
+		},
+	}
+	t.Run("TestGetFaultTime", func(t *testing.T) {
+		if got := GetFaultTime(fault, ""); got != Time100Seconds {
+			t.Error("GetFaultTime fail")
+		}
+		fault.FaultTimeAndLevelMap = make(map[string]constant.FaultTimeAndLevel)
+		if got := GetFaultTime(fault, ""); got != constant.DeviceNotFault {
+			t.Error("GetFaultTime fail")
+		}
+	})
+}
+
+func TestFaultCodeJudge(t *testing.T) {
+	t.Run("TestFaultCodeJudgeAic", func(t *testing.T) {
+		if got := IsUceAccompanyFault(constant.AicFaultCode); got == false {
+			t.Error("TestFaultCodeJudgeAic fail")
+		}
+	})
+	t.Run("TestFaultCodeJudgeLinkDownFault", func(t *testing.T) {
+		if got := IsLinkDownFault(constant.LinkDownFaultCode); got == false {
+			t.Error("TestFaultCodeJudgeLinkDownFault fail")
+		}
+	})
+	t.Run("TestFaultCodeJudgeCqeFault", func(t *testing.T) {
+		if got := IsCqeFault(constant.DevCqeFaultCode); got == false {
+			t.Error("TestFaultCodeJudgeCqeFault fail")
+		}
+	})
+	t.Run("TestFaultCodeJudgeUceFault", func(t *testing.T) {
+		if got := IsUceFault(constant.UceFaultCode); got == false {
+			t.Error("TestFaultCodeJudgeUceFault fail")
+		}
+	})
+}
+
+func TestMergeCodeAndRemoveUnhealthy(t *testing.T) {
+	type args struct {
+		advanceDeviceCm constant.AdvanceDeviceFaultCm
+	}
+	tests := []struct {
+		name string
+		args args
+		want constant.AdvanceDeviceFaultCm
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mergeCodeAndRemoveUnhealthy(tt.args.advanceDeviceCm); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("mergeCodeAndRemoveUnhealthy() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAdvanceDeviceCmForNodeMapToString(t *testing.T) {
+	deviceInfoCms := map[string]*constant.DeviceInfo{
+		CmName: OriginalDeviceCm,
+	}
+	t.Run("TestAdvanceDeviceCmForNodeMapToString", func(t *testing.T) {
+		advanceMap := GetAdvanceDeviceCmForNodeMap(deviceInfoCms)
+		orgDeviceCm := make(map[string]*constant.DeviceInfo)
+		util.DeepCopy(&orgDeviceCm, deviceInfoCms)
+		AdvanceDeviceCmForNodeMapToString(advanceMap, orgDeviceCm)
+		if !reflect.DeepEqual(GetAdvanceDeviceCmForNodeMap(orgDeviceCm), GetAdvanceDeviceCmForNodeMap(deviceInfoCms)) {
+			t.Error("TestAdvanceDeviceCmForNodeMapToString fail")
+		}
+	})
+}
+
+func TestAddFaultAndDeleteFaultMap(t *testing.T) {
+	addFault := constant.DeviceFault{
+		NPUName: DeviceName,
+	}
+	t.Run("TestAddFaultIntoFaultMap", func(t *testing.T) {
+		faultMap := AddFaultIntoFaultMap(nil, addFault)
+		if len(faultMap[addFault.NPUName]) != 1 {
+			t.Error("TestAddFaultIntoFaultMap fail")
+		}
+	})
+	t.Run("TestDeleteFaultFromFaultMap", func(t *testing.T) {
+		faultMap := DeleteFaultFromFaultMap(nil, addFault)
+		if len(faultMap[addFault.NPUName]) != 0 {
+			t.Error("TestDeleteFaultFromFaultMap fail")
+		}
+		faultMap = AddFaultIntoFaultMap(nil, addFault)
+		faultMap = DeleteFaultFromFaultMap(faultMap, addFault)
+		if len(faultMap[addFault.NPUName]) != 0 {
+			t.Error("TestDeleteFaultFromFaultMap fail")
+		}
+	})
+}
+
+func TestGetAdvanceDeviceCmForNodeMap(t *testing.T) {
+	deviceInfoCms := map[string]*constant.DeviceInfo{
+		CmName: OriginalDeviceCm,
+	}
+	t.Run("TestGetAdvanceDeviceConfigmap", func(t *testing.T) {
+		got := GetAdvanceDeviceCmForNodeMap(deviceInfoCms)
+		if len(got[NodeName].FaultDeviceList[DeviceName]) != 2 {
+			t.Error("TestGetAdvanceDeviceConfigmap fail")
+		}
+	})
+}
+
+func TestGetNodeAndDeviceFromJobIdAndRankId(t *testing.T) {
+	t.Run("TestGetNodeAndDeviceFromJobIdAndRankId", func(t *testing.T) {
+		serverName, deviceId, err := GetNodeAndDeviceFromJobIdAndRankId(JobId, RankID, JobServerMap)
+		if serverName != NodeName || deviceId != DeviceId || err != nil {
+			t.Error("TestGetNodeAndDeviceFromJobIdAndRankId fail")
+		}
+	})
+}
+
+func TestIsNodeReady(t *testing.T) {
+	node := &v1.Node{
+		Status: v1.NodeStatus{
+			Conditions: []v1.NodeCondition{{
+				Type:   v1.NodeReady,
+				Status: v1.ConditionTrue,
+			}},
+		},
+	}
+	t.Run("TestIsNodeReady", func(t *testing.T) {
+		if !IsNodeReady(node) {
+			t.Error("TestIsNodeReady fail")
+		}
+	})
+}
