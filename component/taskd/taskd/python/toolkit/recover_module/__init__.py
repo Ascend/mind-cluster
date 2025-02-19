c5e53b9293cca870a6d9a811ea041144f4b2059f
Lianjun Zhang Atlas <zhanglianjun1@huawei.com>
2025-02-19 20:43:58 +0800

    dt修改 & 检视修改

--- component/taskd/taskd/python/toolkit/recover_module/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
-__all__ = [
-    "DLRecoverManager"
-]
-
-from taskd.python.toolkit.recover_module.recover_manager import DLRecoverManager
