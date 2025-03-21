3eddeac245b970c069b92dbbda63d20c24c6e301
liujp20 <3058705262@qq.com>
2025-03-21 16:07:14 +0800

    add DT

--- component/ascend-faultdiag-online/pkg/utils/utils_test.go
+++ component/ascend-faultdiag-online/pkg/utils/utils_test.go
@@ -18,8 +18,9 @@ limitations under the License.
 package utils
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestToFloat64(t *testing.T) {
