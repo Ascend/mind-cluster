85ba2c81ed94ee587ae60db2a6363e6978bd0724
Wu,Qiang-Roy <wuqiangroy@live.com>
2025-05-21 07:54:29 +0000

    !907 add go.mod and go.sum * add go.mod & go.sum

---
+++ component/ascend-faultdiag-online/pkg/service/servicefunc/slownode/constants.go
@@ -0,0 +1,53 @@
+/* Copyright(C) 2025. Huawei Technologies Co.,Ltd. All rights reserved.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+// Package slownode a constants parameters for slownode
+package slownode
+
+const (
+	// DeleteOperator informer operator
+	DeleteOperator = "delete"
+	// AddOperator informer operator
+	AddOperator = "add"
+	// UpdateOperator informer operator
+	UpdateOperator = "update"
+	// SlowNodeOn start slow node feature
+	SlowNodeOn = 1
+	// SlowNodeOff stop slow node feature
+	SlowNodeOff = 0
+	// FileMode write file mode
+	FileMode = 0644
+
+	defaultNamespace = "default"
+	jobSummaryPrefix = "job-summary-"
+	maxRetryCount    = 5
+
+	keyJobName = "training.kubeflow.org/job-name"
+
+	keyJobId     = "job_id"
+	keyJobStatus = "job_status"
+	isRunning    = "running"
+
+	// NodeLevelDetectionResult is the path where store the tp/pp/ slow node algo result
+	nodeLevelDetectionResult = "NodeLevelDetectionResult"
+
+	slownodeAlgoResultSuffix = "_Result.json"
+	slownodeAlgoResultPrefix = "slownode_"
+	parallelGroupSuffix      = "_parallel_group.json"
+
+	start = "start"
+	stop  = "stop"
+
+	success = "success"
+)
---
+++ component/ascend-faultdiag-online/pkg/utils/constants/constants.go
@@ -0,0 +1,68 @@
+/* Copyright(C) 2025. Huawei Technologies Co.,Ltd. All rights reserved.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+// Package constants provides some constants code
+package constants
+
+const (
+	// TypeSeparator character -
+	TypeSeparator = "-"
+	// ValueSeparator character :
+	ValueSeparator = ":"
+)
+
+const (
+	// MetricBandwidth fixed name: bandwidth
+	MetricBandwidth = "bandwidth"
+)
+
+const (
+	// ApiSeparator character /
+	ApiSeparator = "/"
+)
+
+const (
+	// TaskId the id of pingmesh task
+	TaskId = "TaskID"
+	// MinLossRate the mini loss rate of network
+	MinLossRate = "MinLossRate"
+	// MaxLossRate the max loss rate of network
+	MaxLossRate = "MaxLossRate"
+	// AvgLossRate the avg loss rate of network
+	AvgLossRate = "AvgLossRate"
+	// MinDelay the min time delay of network
+	MinDelay = "MinDelay"
+	// MaxDelay the max time delay of network
+	MaxDelay = "MaxDelay"
+	// AvgDelay the avg time delay of network
+	AvgDelay = "AvgDelay"
+	// FaultType the fault type of network
+	FaultType = "FaultType"
+	// SrcID source ip
+	SrcID = "SrcID"
+	// SrcType the type of source ip
+	SrcType = "SrcType"
+	// DstID dest ip
+	DstID = "DstID"
+	// DstType the type of dest ip
+	DstType = "DstType"
+	// Level the level of fault
+	Level = "Level"
+	// XdlIpField the ip env variable of node
+	XdlIpField = "XDL_IP"
+	// PodIP is the ip env variable of cluster
+	PodIP = "POD_IP"
+	// GrpcPort is the port of Grpc server
+	GrpcPort = ":8899"
+)
