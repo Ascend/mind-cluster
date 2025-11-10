ba0c5cd4e576a4b3f5e76f3c2a0bd406bbd34aa1
wangtianyul <502934064@qq.com>
2025-11-10 10:14:29 +0800

    A5ranktable生成相关代码反合 Operator和volcano部分 - part2
    Co-authored-by: wangtianyul<502934064@qq.com>

---
+++ component/ascend-operator/pkg/ranktable/v2dot0/ranktable.go
@@ -0,0 +1,367 @@
+/*
+Copyright(C) 2025. Huawei Technologies Co.,Ltd. All rights reserved.
+*/
+
+// Package v2dot0 is using for v2.0 Ranktable.
+package v2dot0
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+
+	corev1 "k8s.io/api/core/v1"
+
+	"ascend-common/api"
+	"ascend-common/common-utils/hwlog"
+	"ascend-operator/pkg/api/v1"
+	"ascend-operator/pkg/ranktable/common"
+	"ascend-operator/pkg/ranktable/utils"
+)
+
+const (
+	rankTableVersion = "2.0"
+)
+
+const (
+	// level0 for ranktable
+	level0 = 0
+	// level1 for ranktable
+	level1 = 1
+	// level2 for ranktable
+	level2 = 2
+	// level3 for ranktable
+	level3 = 3
+	// index of the first device in the devices list
+	firstDevice = 0
+	// represents zero length or empty value check
+	zero = 0
+)
+
+// NetInfo for net info of the item in rank table
+type NetInfo struct {
+	PortAddrType string
+	ScaleOutType string
+	RankAddrType string
+}
+
+var defaultPriorityScaleOutType = []string{
+	v1.PortAddrTypeRoCE,
+	v1.PortAddrTypeUBoE,
+	v1.PortAddrTypeUBG,
+}
+
+var portTypeMappings = map[string]NetInfo{
+	v1.PortAddrTypeRoCE: {
+		PortAddrType: v1.PortAddrTypeRoCE, ScaleOutType: v1.ScaleOutTypeRoCE, RankAddrType: v1.RankAddrTypeIP,
+	},
+	v1.PortAddrTypeUBoE: {
+		PortAddrType: v1.PortAddrTypeUBoE, ScaleOutType: v1.ScaleOutTypeUBoE, RankAddrType: v1.RankAddrTypeIP,
+	},
+	v1.PortAddrTypeUBG: {
+		PortAddrType: v1.PortAddrTypeUBG, ScaleOutType: v1.ScaleOutTypeUBoE, RankAddrType: v1.RankAddrTypeEID,
+	},
+}
+
+var scaleOutTypeMappings = map[string][]NetInfo{
+	v1.ScaleOutTypeRoCE: {
+		{PortAddrType: v1.PortAddrTypeRoCE, ScaleOutType: v1.ScaleOutTypeRoCE, RankAddrType: v1.RankAddrTypeIP},
+	},
+	v1.ScaleOutTypeUBoE: {
+		{PortAddrType: v1.PortAddrTypeUBoE, ScaleOutType: v1.ScaleOutTypeUBoE, RankAddrType: v1.RankAddrTypeIP},
+		{PortAddrType: v1.PortAddrTypeUBG, ScaleOutType: v1.ScaleOutTypeUBoE, RankAddrType: v1.RankAddrTypeEID},
+	},
+}
+
+// GetNetInfoByDefault get rank info generating policy by default priority
+func (r *RankTable) GetNetInfoByDefault() (NetInfo, error) {
+	for _, sot := range defaultPriorityScaleOutType {
+		if _, exist := r.portAddrTypes.Load(sot); !exist {
+			continue
+		}
+
+		if netInfo, exist := portTypeMappings[sot]; exist {
+			return netInfo, nil
+		}
+	}
+	// if no matching network type is found, log a warning and still generate the rank table with empty fields.
+	hwlog.RunLog.Warn("no suitable port addr type found")
+	return NetInfo{}, nil
+}
+
+// GetNetInfoByCustom get rank info generating policy by user custom
+func (r *RankTable) GetNetInfoByCustom() (NetInfo, error) {
+	infoList, exist := scaleOutTypeMappings[r.customScaleOutType]
+	if !exist {
+		errMsg := fmt.Sprintf("the value of label %s is invalid, which should be %s or %s",
+			v1.ScaleOutTypeLabel, v1.ScaleOutTypeRoCE, v1.ScaleOutTypeUBoE)
+		// In a priority scenario, if the scale-out type is invalid, log an error and skip generating the rank table.
+		hwlog.RunLog.Error(errMsg)
+		return NetInfo{}, errors.New(errMsg)
+	}
+	for _, item := range infoList {
+		if _, ok := r.portAddrTypes.Load(item.PortAddrType); ok {
+			return item, nil
+		}
+	}
+	// if no matching network type is found, log a warning and still generate the rank table with empty fields.
+	hwlog.RunLog.Warnf("no suitable port addr type found in device for the custom %v label value %s",
+		v1.ScaleOutTypeLabel, r.customScaleOutType)
+	return NetInfo{}, nil
+}
+
+// GetNetInfo get rank info generating policy
+func (r *RankTable) GetNetInfo() (NetInfo, error) {
+	if len(r.customScaleOutType) == 0 {
+		return r.GetNetInfoByDefault()
+	}
+	return r.GetNetInfoByCustom()
+}
+
+// RankTable is the struct for rank table file
+type RankTable struct {
+	*common.BaseGenerator
+	ranks     *sync.Map
+	RankList  []*common.Rank `json:"rank_list,omitempty"`  // for latest 910A5 ranktable
+	RankCount int            `json:"rank_count,omitempty"` // for latest 910A5 ranktable
+	// portAddrTypes for collect the port addr type info of the rank table 910A5
+	portAddrTypes      *sync.Map
+	customScaleOutType string
+}
+
+// New construct the instance of RankTable
+func New(job *v1.AscendJob) *RankTable {
+	r := &RankTable{
+		ranks:              &sync.Map{},
+		RankList:           []*common.Rank{},
+		portAddrTypes:      &sync.Map{},
+		customScaleOutType: "",
+	}
+	r.BaseGenerator = common.NewBaseGenerator(job, rankTableVersion, r)
+	if scaleOutType, ok := job.Labels[v1.ScaleOutTypeLabel]; ok {
+		r.customScaleOutType = strings.ToUpper(strings.TrimSpace(scaleOutType))
+	}
+	return r
+}
+
+// AddPod for add pod in rank table
+func (r *RankTable) AddPod(pod *corev1.Pod) error {
+	if pod == nil {
+		hwlog.RunLog.Error("illegal input, pod is nil")
+		return errors.New("illegal input, pod is nil")
+	}
+	deviceInfo, ok := pod.Annotations[api.Pod910DeviceAnno]
+	if !ok {
+		// Key does not exist, handle explicitly
+		return fmt.Errorf("annotation %s not found in pod", api.Pod910DeviceAnno)
+	}
+	var instance common.Instance
+	if err := json.Unmarshal([]byte(deviceInfo), &instance); err != nil {
+		hwlog.RunLog.Errorf("unmarshal pod(%s/%s) deviceInfo(%s) failed: %v", pod.Namespace, pod.Name,
+			deviceInfo, err)
+		return err
+	}
+	if pod.Status.PodIP == "" {
+		hwlog.RunLog.Errorf("pod(%s/%s) ip is empty", pod.Namespace, pod.Name)
+		return fmt.Errorf("pod(%s/%s) ip is empty", pod.Namespace, pod.Name)
+	}
+	if len(instance.Devices) == zero || len(instance.Devices[firstDevice].LevelList) == zero {
+		hwlog.RunLog.Errorf("pod(%s/%s) data is incomplete", pod.Namespace, pod.Name)
+		return fmt.Errorf("pod(%s/%s) data is incomplete", pod.Namespace, pod.Name)
+	}
+	// Save the server_list value to baseGenerator
+	if err := r.BaseGenerator.AddPod(pod); err != nil {
+		hwlog.RunLog.Errorf("pod(%s/%s) add failed.Error: %s", pod.Namespace, pod.Name, err)
+		return fmt.Errorf("pod(%s/%s) add failed.Error: %s", pod.Namespace, pod.Name, err)
+	}
+
+	return addPodInSuperPodScene(pod, r, instance)
+}
+
+func recordDevicePortAddrTypes(r *RankTable, dev common.Dev) {
+	for _, rankLevel := range dev.LevelList {
+		for netType := range rankLevel.Info {
+			if _, exist := r.portAddrTypes.Load(netType); !exist {
+				hwlog.RunLog.Infof("the rank table file contains device port addr type: %v", netType)
+				r.portAddrTypes.Store(netType, struct{}{})
+			}
+		}
+	}
+}
+
+func addPodInSuperPodScene(pod *corev1.Pod, r *RankTable, instance common.Instance) error {
+	rankIndex, err := strconv.Atoi(pod.Annotations[api.PodRankIndexAnno])
+	if err != nil {
+		hwlog.RunLog.Errorf("parse pod(%s/%s) rankIndex(%s) failed: %v", pod.Namespace, pod.Name,
+			pod.Annotations[api.PodRankIndexAnno], err)
+		return err
+	}
+
+	devices := instance.Devices
+	rankFactor := len(devices)
+	podRank := make([]*common.Rank, 0, rankFactor)
+
+	for _, dev := range devices {
+		recordDevicePortAddrTypes(r, dev)
+	}
+
+	for index := range devices {
+		var rank common.Rank
+		if errTidy := GenRankList(&rank, &instance, index); errTidy != nil {
+			hwlog.RunLog.Errorf("parse data failed: %v", errTidy)
+			return errTidy
+		}
+		rank.RankID = rankIndex*rankFactor + index
+		podRank = append(podRank, &rank)
+	}
+
+	if len(podRank) < 1 {
+		return fmt.Errorf("%s/%s get device list failed", pod.Namespace, pod.Name)
+	}
+	r.ranks.Store(pod.UID, podRank)
+	return nil
+}
+
+// shouldInclude determines whether the given level should be retained.
+func shouldInclude(level int, portAddrType, customScaleOutType string) bool {
+	// If portAddrType is empty and customScaleOutType is set, use customScaleOutType instead.
+	if portAddrType == "" && customScaleOutType != "" {
+		portAddrType = customScaleOutType
+	}
+
+	switch level {
+	case level2:
+		// Retain only if portAddrType is UBoE or UBG.
+		return portAddrType == v1.PortAddrTypeUBoE || portAddrType == v1.PortAddrTypeUBG
+	case level3:
+		// Retain only if portAddrType is RoCE.
+		return portAddrType == v1.PortAddrTypeRoCE
+	default:
+		// Retain by default.
+		return true
+	}
+}
+
+// getElement retrieves the element for a given level.
+func getElement(curDevice common.Dev, level int, portAddrType string) (api.LevelElement, bool) {
+	// Iterate through LevelList to find the layer where Level == target level.
+	for _, lvl := range curDevice.LevelList {
+		if lvl.Level != level {
+			continue
+		}
+
+		// For level 0 and 1: directly return the first element.
+		if level == level0 || level == level1 {
+			for _, v := range lvl.Info {
+				return v, true
+			}
+			return api.LevelElement{}, false
+		}
+
+		// For other levels: retrieve by portAddrType.
+		elem, ok := lvl.Info[portAddrType]
+		return elem, ok
+	}
+
+	return api.LevelElement{}, false
+}
+
+// GenRankList initializes and populates the rank structure with level0 and level1 information.
+func GenRankList(rank *common.Rank, instance *common.Instance, index int) error {
+	curDevice := instance.Devices[index]
+	rank.Device = curDevice
+
+	// Parse DeviceID.
+	localID, err := strconv.Atoi(curDevice.DeviceID)
+	if err != nil {
+		hwlog.RunLog.Errorf("parse device id(%s) failed: %v", curDevice.DeviceID, err)
+		return err
+	}
+	rank.LocalID = localID
+	rank.DeviceID = localID
+
+	// Process only level0 and level1.
+	for level := level0; level <= level1; level++ {
+		elem, ok := getElement(curDevice, level, "")
+		if !ok {
+			// Handle missing level.
+			hwlog.RunLog.Warnf("device %s level=%d has no valid element, skip append",
+				curDevice.DeviceID, level)
+			continue
+		}
+		rank.LevelList = append(rank.LevelList, elem)
+	}
+
+	return nil
+}
+
+// DeletePod clears rank-related data.
+func (r *RankTable) DeletePod() {
+	r.ranks = &sync.Map{}
+	r.portAddrTypes = &sync.Map{}
+	r.SetStatus(utils.InitialRTStatus)
+}
+
+func (r *RankTable) GatherServerList() {
+	r.RankList = make([]*common.Rank, 0)
+	r.ranks.Range(func(key, value interface{}) bool {
+		if ranks, ok := value.([]*common.Rank); ok {
+			for _, rank := range ranks {
+				r.RankList = append(r.RankList, rank)
+			}
+		} else {
+			hwlog.RunLog.Warnf("unexpected type in ranks map for key=%v: %T", key, value)
+		}
+		return true
+	})
+
+	// Retrieve portAddrType.
+	var portAddrType string
+	if netInfo, err := r.GetNetInfo(); err != nil {
+		hwlog.RunLog.Warnf("GetNetInfo failed in GatherServerList: %v", err)
+		r.SetNeedGenerate(false)
+		portAddrType = ""
+	} else {
+		portAddrType = netInfo.PortAddrType
+		hwlog.RunLog.Infof("finally selected result in GatherServerList: scaleOutType=%s, portAddrType=%s",
+			netInfo.ScaleOutType, portAddrType)
+	}
+
+	// Iterate through ranks and supplement level2 / level3.
+	for _, rank := range r.RankList {
+		curDevice := rank.Device
+
+		for _, level := range []int{level2, level3} {
+			if !shouldInclude(level, portAddrType, r.customScaleOutType) {
+				continue
+			}
+
+			// Retrieve element; if not found, use default values.
+			elem, ok := getElement(curDevice, level, portAddrType)
+			if !ok {
+				hwlog.RunLog.Warnf("device %s level=%d has no valid element, using empty default",
+					curDevice.DeviceID, level)
+				elem = api.LevelElement{
+					NetLayer:      level,
+					NetInstanceID: api.DefaultClusterName,
+					NetType:       api.NetTypeCLOS,
+					NetAttr:       api.NetAttrEmpty,
+					RankAddrList:  []api.RankAddrItem{},
+				}
+			}
+			rank.LevelList = append(rank.LevelList, elem)
+		}
+	}
+
+	// Sort and count ranks.
+	sort.Slice(r.RankList, func(i, j int) bool {
+		iRankID := r.RankList[i].RankID
+		jRankID := r.RankList[j].RankID
+		return iRankID < jRankID
+	})
+	r.RankCount = len(r.RankList)
+}
---
+++ component/ascend-operator/pkg/ranktable/v2dot0/ranktable_test.go
@@ -0,0 +1,429 @@
+/*
+Copyright(C) 2024. Huawei Technologies Co.,Ltd. All rights reserved.
+*/
+
+/*
+Package v2dot0 is used to test generate ranktable in v2.0
+*/
+package v2dot0
+
+import (
+	"encoding/json"
+	"sync"
+	"testing"
+
+	"github.com/smartystreets/goconvey/convey"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"ascend-common/api"
+	"ascend-operator/pkg/api/v1"
+	"ascend-operator/pkg/ranktable/common"
+)
+
+// TestGetNetInfoByDefault test case for GetNetInfoByDefault
+func TestGetNetInfoByDefault(t *testing.T) {
+	convey.Convey("TestGetNetInfoByDefault", t, func() {
+		convey.Convey("01-should return empty when portAddrTypes is empty", func() {
+			r := &RankTable{
+				portAddrTypes: &sync.Map{},
+			}
+			expected := NetInfo{}
+			res, err := r.GetNetInfoByDefault()
+			convey.So(err, convey.ShouldBeNil)
+			convey.So(res, convey.ShouldResemble, expected)
+		})
+		convey.Convey("02-should return roce info when portAddrTypes contain roce info", func() {
+			r := &RankTable{
+				portAddrTypes: &sync.Map{},
+			}
+			r.portAddrTypes.Store(v1.PortAddrTypeRoCE, struct{}{})
+			expected := NetInfo{
+				PortAddrType: v1.PortAddrTypeRoCE, ScaleOutType: v1.ScaleOutTypeRoCE, RankAddrType: v1.RankAddrTypeIP,
+			}
+			res, err := r.GetNetInfoByDefault()
+			convey.So(err, convey.ShouldBeNil)
+			convey.So(res, convey.ShouldResemble, expected)
+		})
+		convey.Convey("03-should return uboe info when portAddrTypes contain uboe info", func() {
+			r := &RankTable{
+				portAddrTypes: &sync.Map{},
+			}
+			r.portAddrTypes.Store(v1.PortAddrTypeUBoE, struct{}{})
+			expected := NetInfo{
+				PortAddrType: v1.PortAddrTypeUBoE, ScaleOutType: v1.ScaleOutTypeUBoE, RankAddrType: v1.RankAddrTypeIP,
+			}
+			res, err := r.GetNetInfoByDefault()
+			convey.So(err, convey.ShouldBeNil)
+			convey.So(res, convey.ShouldResemble, expected)
+		})
+		convey.Convey("04-should return ubg info when portAddrTypes contain ubg info", func() {
+			r := &RankTable{
+				portAddrTypes: &sync.Map{},
+			}
+			r.portAddrTypes.Store(v1.PortAddrTypeUBG, struct{}{})
+			expected := NetInfo{
+				PortAddrType: v1.PortAddrTypeUBG, ScaleOutType: v1.ScaleOutTypeUBoE, RankAddrType: v1.RankAddrTypeEID,
+			}
+			res, err := r.GetNetInfoByDefault()
+			convey.So(err, convey.ShouldBeNil)
+			convey.So(res, convey.ShouldResemble, expected)
+		})
+	})
+}
+
+// TestGetNetInfoByCustom test case for GetNetInfoByCustom
+func TestGetNetInfoByCustom(t *testing.T) {
+	convey.Convey("TestGetNetInfoByCustom", t, func() {
+		convey.Convey("00-should return empty when portAddrTypes is empty", func() {
+			r := &RankTable{
+				customScaleOutType: v1.ScaleOutTypeRoCE,
+				portAddrTypes:      &sync.Map{},
+			}
+			expected := NetInfo{}
+			res, err := r.GetNetInfoByCustom()
+			convey.So(err, convey.ShouldBeNil)
+			convey.So(res, convey.ShouldResemble, expected)
+		})
+		convey.Convey("01-should return error when scaleout-type is invalid", func() {
+			r := &RankTable{
+				customScaleOutType: "XXX",
+				portAddrTypes:      &sync.Map{},
+			}
+			expected := NetInfo{}
+			res, err := r.GetNetInfoByCustom()
+			convey.So(err, convey.ShouldNotBeNil)
+			convey.So(res, convey.ShouldResemble, expected)
+		})
+		testGetNetInfoByCustomWhenScaleOutTypeExists()
+	})
+}
+
+func testGetNetInfoByCustomWhenScaleOutTypeExists() {
+	convey.Convey("02-return roce info when scaleout-type=roce and portAddrTypes contain roce info", func() {
+		r := &RankTable{
+			customScaleOutType: v1.ScaleOutTypeRoCE,
+			portAddrTypes:      &sync.Map{},
+		}
+		r.portAddrTypes.Store(v1.PortAddrTypeRoCE, struct{}{})
+		expected := NetInfo{
+			PortAddrType: v1.PortAddrTypeRoCE, ScaleOutType: v1.ScaleOutTypeRoCE, RankAddrType: v1.RankAddrTypeIP,
+		}
+		res, err := r.GetNetInfoByCustom()
+		convey.So(err, convey.ShouldBeNil)
+		convey.So(res, convey.ShouldResemble, expected)
+	})
+	convey.Convey("03-return uboe info when scaleout-type=uboe and portAddrTypes contain uboe info", func() {
+		r := &RankTable{
+			customScaleOutType: v1.ScaleOutTypeUBoE,
+			portAddrTypes:      &sync.Map{},
+		}
+		r.portAddrTypes.Store(v1.PortAddrTypeUBoE, struct{}{})
+		expected := NetInfo{
+			PortAddrType: v1.PortAddrTypeUBoE, ScaleOutType: v1.ScaleOutTypeUBoE, RankAddrType: v1.RankAddrTypeIP,
+		}
+		res, err := r.GetNetInfoByCustom()
+		convey.So(err, convey.ShouldBeNil)
+		convey.So(res, convey.ShouldResemble, expected)
+	})
+	convey.Convey("04-return ubg info when scaleout-type=uboe and portAddrTypes contain ubg info", func() {
+		r := &RankTable{
+			customScaleOutType: v1.ScaleOutTypeUBoE,
+			portAddrTypes:      &sync.Map{},
+		}
+		r.portAddrTypes.Store(v1.PortAddrTypeUBG, struct{}{})
+		expected := NetInfo{
+			PortAddrType: v1.PortAddrTypeUBG, ScaleOutType: v1.ScaleOutTypeUBoE, RankAddrType: v1.RankAddrTypeEID,
+		}
+		res, err := r.GetNetInfoByCustom()
+		convey.So(err, convey.ShouldBeNil)
+		convey.So(res, convey.ShouldResemble, expected)
+	})
+}
+
+// Construct a fake Instance JSON
+func fakeInstanceJSON() string {
+	inst := common.Instance{
+		PodName: "test-pod",
+		Devices: []common.Dev{
+			{
+				DeviceID: "0",
+				DeviceIP: "192.168.0.1",
+				LevelList: []api.RankLevel{
+					{Level: level0, Info: map[string]api.LevelElement{
+						v1.PortAddrTypeUB: {NetLayer: level0, NetInstanceID: "L0"},
+					}},
+					{Level: level1, Info: map[string]api.LevelElement{
+						v1.PortAddrTypeUB: {NetLayer: level1, NetInstanceID: "L1"},
+					}},
+					{Level: level2, Info: map[string]api.LevelElement{
+						v1.ScaleOutTypeUBoE: {NetLayer: level2, NetInstanceID: "L2-UBOE"},
+					}},
+					{Level: level3, Info: map[string]api.LevelElement{
+						v1.PortAddrTypeRoCE: {NetLayer: level3, NetInstanceID: "L3-ROCE"},
+					}},
+				},
+			},
+		},
+	}
+	b, err := json.Marshal(inst)
+	if err != nil {
+		return ""
+	}
+	return string(b)
+}
+
+func fakeAscendJob() *v1.AscendJob {
+	return &v1.AscendJob{
+		ObjectMeta: metav1.ObjectMeta{
+			Labels: map[string]string{},
+		},
+	}
+}
+
+// TestNewRankTable verifies that creating a new RankTable from a minimal AscendJob
+// returns a non-nil RankTable with its RankList properly initialized.
+func TestNewRankTable(t *testing.T) {
+	// Construct a minimal AscendJob
+	job := fakeAscendJob()
+
+	rt := New(job)
+	if rt == nil {
+		t.Fatal("expected RankTable, got nil")
+	}
+	if rt.RankList == nil {
+		t.Error("expected RankList initialized")
+	}
+}
+
+// TestAddPodAndGather checks that adding a Pod updates the RankTable correctly.
+func TestAddPodAndGather(t *testing.T) {
+	// Construct a fake Pod
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-pod",
+			Namespace: "default",
+			UID:       "uid-123",
+			Annotations: map[string]string{
+				api.Pod910DeviceAnno: fakeInstanceJSON(),
+				api.PodRankIndexAnno: "0",
+			},
+		},
+		Status: corev1.PodStatus{
+			PodIP: "10.0.0.1",
+		},
+	}
+
+	rt := New(fakeAscendJob())
+
+	// Call AddPod
+	if err := rt.AddPod(pod); err != nil {
+		t.Fatalf("AddPod failed: %v", err)
+	}
+
+	// Gather RankList
+	rt.GatherServerList()
+	if rt.RankCount != 1 {
+		t.Errorf("expected RankCount=1, got %d", rt.RankCount)
+	}
+	if len(rt.RankList) != 1 {
+		t.Fatalf("expected RankList length=1, got %d", len(rt.RankList))
+	}
+}
+
+// TestShouldInclude verifies the inclusion logic for different levels and address types.
+func TestShouldInclude(t *testing.T) {
+	if !shouldInclude(level2, v1.ScaleOutTypeUBoE, "") {
+		t.Error("expected level=2 with UBOE to be included")
+	}
+	if shouldInclude(level2, v1.PortAddrTypeUB, "") {
+		t.Error("expected level=2 with FOO to be excluded")
+	}
+	if !shouldInclude(level3, v1.ScaleOutTypeRoCE, "") {
+		t.Error("expected level=3 with ROCE to be included")
+	}
+	if shouldInclude(level3, v1.ScaleOutTypeUBoE, "") {
+		t.Error("expected level=3 with UBOE to be excluded")
+	}
+}
+
+// TestGetElement checks that getElement retrieves the correct LevelElement from a device.
+func TestGetElement(t *testing.T) {
+	dev := common.Dev{
+		DeviceID: "0",
+		LevelList: []api.RankLevel{
+			{Level: 0, Info: map[string]api.LevelElement{
+				"UB": {NetLayer: 0, NetInstanceID: "L0"},
+			}},
+		},
+	}
+	elem, ok := getElement(dev, 0, "UB")
+	if !ok || elem.NetInstanceID != "L0" {
+		t.Errorf("expected element L0, got %+v", elem)
+	}
+}
+
+// TestDeletePod checks that calling DeletePod reinitializes the internal maps in RankTable.
+func TestDeletePod(t *testing.T) {
+	rt := New(fakeAscendJob())
+	rt.DeletePod()
+	if rt.ranks == nil || rt.portAddrTypes == nil {
+		t.Error("expected maps reinitialized")
+	}
+}
+
+// genRankListForTest is a test-only helper that allows specifying portAddrType directly.
+func genRankListForTest(inst *common.Instance, portAddrType string) *common.Rank {
+	rank := &common.Rank{}
+	curDevice := inst.Devices[0]
+
+	for level := 0; level < 4; level++ {
+		if !shouldInclude(level, portAddrType, "") {
+			continue
+		}
+		elem, ok := getElement(curDevice, level, portAddrType)
+		if !ok {
+			// Fallback to default values when element is not found
+			elem = api.LevelElement{
+				NetLayer:      level,
+				NetInstanceID: "CLUSTER",
+				NetType:       "CLOS",
+				NetAttr:       "",
+			}
+		}
+		rank.LevelList = append(rank.LevelList, elem)
+	}
+	return rank
+}
+
+func newTestInstanceForGenRank() common.Instance {
+	return common.Instance{
+		PodName: "test-pod",
+		Devices: []common.Dev{
+			{
+				DeviceID: "0",
+				DeviceIP: "192.168.0.1",
+				LevelList: []api.RankLevel{
+					{Level: level0, Info: map[string]api.LevelElement{
+						v1.PortAddrTypeUB: {NetLayer: level0, NetInstanceID: "L0"},
+					}},
+					{Level: level1, Info: map[string]api.LevelElement{
+						v1.PortAddrTypeUB: {NetLayer: level1, NetInstanceID: "L1"},
+					}},
+					{Level: level2, Info: map[string]api.LevelElement{
+						v1.PortAddrTypeUBoE: {NetLayer: level2, NetInstanceID: "L2-UBOE"},
+						v1.PortAddrTypeUBG:  {NetLayer: level2, NetInstanceID: "L2-UBG"},
+					}},
+					{Level: level3, Info: map[string]api.LevelElement{
+						v1.PortAddrTypeRoCE: {NetLayer: level3, NetInstanceID: "L3-ROCE"},
+					}},
+				},
+			},
+		},
+	}
+}
+
+func assertRankList(t *testing.T, got []api.LevelElement, expectedLayers []int, expectedIDs []string) {
+	t.Helper()
+
+	minLen := len(got)
+	if len(expectedLayers) < minLen {
+		minLen = len(expectedLayers)
+	}
+	if len(expectedIDs) < minLen {
+		minLen = len(expectedIDs)
+	}
+
+	for i := 0; i < minLen; i++ {
+		if got[i].NetLayer != expectedLayers[i] {
+			t.Errorf("expected NetLayer=%d at index %d, got %d",
+				expectedLayers[i], i, got[i].NetLayer)
+		}
+		if got[i].NetInstanceID != expectedIDs[i] {
+			t.Errorf("expected NetInstanceID=%s at index %d, got %s",
+				expectedIDs[i], i, got[i].NetInstanceID)
+		}
+	}
+
+	if len(got) != len(expectedLayers) || len(got) != len(expectedIDs) {
+		t.Fatalf("length mismatch: got=%d, expectedLayers=%d, expectedIDs=%d",
+			len(got), len(expectedLayers), len(expectedIDs))
+	}
+}
+
+// TestGenRankList_DifferentPortAddrTypes verifies that genRankListForTest
+// produces the expected RankList for different port address types.
+func TestGenRankList_DifferentPortAddrTypes(t *testing.T) {
+	inst := newTestInstanceForGenRank()
+
+	tests := []struct {
+		name           string
+		portAddrType   string
+		expectedLayers []int
+		expectedIDs    []string
+	}{
+		{"UBOE allowed at L2", v1.PortAddrTypeUBoE,
+			[]int{0, 1, 2}, []string{"L0", "L1", "L2-UBOE"}},
+		{"UBG allowed at L2", v1.PortAddrTypeUBG,
+			[]int{0, 1, 2}, []string{"L0", "L1", "L2-UBG"}},
+		{"ROCE allowed at L3", v1.PortAddrTypeRoCE,
+			[]int{0, 1, 3}, []string{"L0", "L1", "L3-ROCE"}},
+		{"Other type skip L2/L3", v1.PortAddrTypeUB,
+			[]int{0, 1}, []string{"L0", "L1"}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rank := genRankListForTest(&inst, tt.portAddrType)
+			assertRankList(t, rank.LevelList, tt.expectedLayers, tt.expectedIDs)
+		})
+	}
+}
+
+// TestAddPodErrors checks that AddPod returns errors for invalid Pod inputs.
+func TestAddPodErrors(t *testing.T) {
+	rt := New(fakeAscendJob())
+
+	// nil Pod
+	if err := rt.AddPod(nil); err == nil {
+		t.Error("expected error when pod is nil")
+	}
+
+	// Pod without IP
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "no-ip",
+			Annotations: map[string]string{
+				api.Pod910DeviceAnno: "{}",
+				api.PodRankIndexAnno: "0",
+			},
+		},
+	}
+	if err := rt.AddPod(pod); err == nil {
+		t.Error("expected error when PodIP is empty")
+	}
+}
+
+// TestGenRankListInvalidDeviceID checks that GenRankList returns an error when the DeviceID is not numeric.
+func TestGenRankListInvalidDeviceID(t *testing.T) {
+	inst := common.Instance{
+		Devices: []common.Dev{
+			{DeviceID: "not-a-number"},
+		},
+	}
+	var rank common.Rank
+	if err := GenRankList(&rank, &inst, 0); err == nil {
+		t.Error("expected error when DeviceID is not numeric")
+	}
+}
+
+// TestGatherServerListSorting checks that GatherServerList sorts the RankList by RankID.
+func TestGatherServerListSorting(t *testing.T) {
+	rt := New(fakeAscendJob())
+	// Simulate two Ranks
+	rt.ranks.Store("uid1", []*common.Rank{{RankID: 2}, {RankID: 1}})
+	rt.GatherServerList()
+	if rt.RankList[0].RankID != 1 {
+		t.Errorf("expected first RankID=1, got %d", rt.RankList[0].RankID)
+	}
+}
