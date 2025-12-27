03f1571c5595e66f7b5bcf86cd0194986d3e5d0b
maybeclear <mengfeilong@huawei.com>
2025-12-27 16:13:10 +0800

    【operator】最大副本数量限制修改为20,000
    Co-authored-by: maybeclear<mengfeilong@huawei.com>

--- component/ascend-operator/pkg/controllers/v1/consts.go
+++ component/ascend-operator/pkg/controllers/v1/consts.go
@@ -121,7 +121,7 @@ const (
 	// labelFaultRetryTimes represents the key of label fault-retry-times.
 	labelFaultRetryTimes = "fault-retry-times"
 	// maxReplicas
-	maxReplicas = 15000
+	maxReplicas = 20000
 )
 
 const (
