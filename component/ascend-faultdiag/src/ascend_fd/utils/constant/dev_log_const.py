c10785269a07f98b2e418e70e0cc49e962ea6973
frankang <goaheada@foxmail.com>
2026-09-07 15:19:09 +0800

    ub链路诊断精细定位代码，第一部分补充故障模式库和常量
    Co-authored-by: frankang<kangfuan2@huawei.com>

---
+++ component/ascend-faultdiag/src/ascend_fd/utils/constant/dev_log_const.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2026 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+HISI_LOG_DIR = "hisi_logs"
+SLOG_DIR = "slog"
+
+BEFORE_TASK_DIR = "before_task"
+AFTER_TASK_DIR = "after_task"
